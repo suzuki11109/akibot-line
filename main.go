bd96630f57b0bcf83c4be21874f405cfee1ec529
Suzuki Aki <suzuki11109@gmail.com>
2016-10-21 16:41:03 +0700

    add example callback handler

--- main.go
+++ main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/line/line-bot-sdk-go/linebot"
 )
 
 func main() {
+	bot, err := linebot.New(
+		os.Getenv("CHANNEL_SECRET"),
+		os.Getenv("CHANNEL_TOKEN"),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello world")
+		events, err := bot.ParseRequest(r)
+		if err != nil {
+			if err == linebot.ErrInvalidSignature {
+				w.WriteHeader(400)
+			} else {
+				w.WriteHeader(500)
+			}
+			return
+		}
+
+		for _, event := range events {
+			if event.Type == linebot.EventTypeMessage {
+				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
+					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
+						log.Print(err)
+					}
+				}
+			}
+		}
 	})
 
 	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
